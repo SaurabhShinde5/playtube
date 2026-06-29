f4fa2cb74c6f1c6c06ee33a5f1666aed0808ca9c
saurabh-sorted <sourabh@sorted.agency>
2026-06-29 16:39:26 +0530

    All setup configure and implement user registration route

--- src/app.js
+++ src/app.js
@@ -13,4 +13,8 @@ app.use(express.urlencoded({ extended: true, limit: "1kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes
+import userRoutes from "./routes/user.routes.js";
+app.use('/api/v1/users', userRoutes);
+
 export default app;
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs/promises";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+    if (!localFilePath) return null;
+
+    try {
+        const result = await cloudinary.uploader.upload(localFilePath, {
+            use_filename: true,
+            unique_filename: false,
+            overwrite: true,
+            resource_type: "auto",
+        });
+
+        await fs.unlink(localFilePath).catch(() => {});
+
+        return result;
+    } catch (error) {
+        console.error("Cloudinary upload failed:", error);
+
+        await fs.unlink(localFilePath).catch(() => {});
+
+        return null;
+    }
+};
